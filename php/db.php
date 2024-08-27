ac31dc7e0d53763df389f52c117f9a41e7e223cb
Alex Daniel Mora Heredia <alex.daniel50904@gmail.com>
2024-08-27 10:51:25 -0500

    second commit - creating crud with php

--- php/db.php
+++ php/db.php
@@ -1,2 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database</title>
+    <link rel="stylesheet" href="../php/styles.css">
+    <link rel="shortcut icon" href="../icons/data-regular-24.png" type="image/x-icon">
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
+echo "date " . date("d/m/y") . "<br>";
+
+$date = date("d/m/y") . "<br>";
 
--- php/view/addView.php
+++ php/view/addView.php
@@ -3,11 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add user</title>
-    
+    <title>Add</title>
+    <link rel="shortcut icon" href="../../icons/user-plus-regular-24.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
 <body>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center my-3">Add to calendar</h1>
+            <div class="col">
+                <div class="col-6 mx-auto">
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" id="radioMale" name="optradio" value="male" checked>
+                        <label class="form-check-label" for="radioMale">Masculino</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" id="radioFemale" name="optradio" value="female">
+                        <label class="form-check-label" for="radioFemale">Femenino</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<?php
+
