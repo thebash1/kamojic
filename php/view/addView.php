c7d42045bcfa4c24c3dcd71e717ac2e9f87ee0f5
Alex Daniel Mora Heredia <alex.daniel50904@gmail.com>
2024-09-08 21:52:39 -0500

    none

--- php/view/addView.php
+++ php/view/addView.php
@@ -1,24 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Añadir</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="../../icons/user-plus-regular-24.png" type="image/x-icon">
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <h1 class="text-center my-3">Add to calendar</h1>
-            <div class="col">
-                <div class="col-6 mx-auto">
-                    <div class="form-check">
-                        <input type="radio" class="form-check-input" id="radioMale" name="optradio" value="male" checked>
-                        <label class="form-check-label" for="radioMale">Masculino</label>
-                    </div>
-                    <div class="form-check">
-                        <input type="radio" class="form-check-input" id="radioFemale" name="optradio" value="female">
-                        <label class="form-check-label" for="radioFemale">Femenino</label>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <section class="main">
+        <h1 class="text-center">Agendar cita</h1>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <div class="form-control">
+            <form>
+                <label for="first_name">Nombre:</label><br>
+                <input type="text" id="first_name" name="first_name"><br>
+                <label for="last_name">Apellido:</label><br>
+                <input type="text" id="last_name" name="last_name">
+            </form>
+        </div>
+    </section>
+
+    
 </body>
 </html>
 
