33a1f8d974c85005763346d7ac00bbefd2f232f6
Alex Daniel Mora Heredia <alex.daniel50904@gmail.com>
2024-09-09 21:53:22 -0500

    without comment

--- php/view/addView.php
+++ php/view/addView.php
@@ -4,22 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Añadir</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
     <link rel="shortcut icon" href="../../icons/user-plus-regular-24.png" type="image/x-icon">
+    
 </head>
 <body>
     <section class="container">
-       <h1 class="h1">Agendar cita</h1>
-       <form action="" class="form">
-            <div class="input-box">
-                <label for="fname">Nombre:</label>
-                <input type="text">
-            </div>
-            <div class="input-box">
-                <label for="lname">Apellido:</label>
-                <input type="text">
-            </div>
-       </form>
+
+        <h1 class="h1">Agendar cita</h1>
+    
+        <form action="" class="form">
+                <div class="input-box">
+                    <label for="fname">Nombre:</label>
+                    <input type="text">
+                </div>
+                <div class="input-box">
+                    <label for="lname">Apellido:</label>
+                    <input type="text">
+                </div>
+        </form>
        <button type="button" id="toggleButton"><img src="../../icons/moon-regular-24.png" alt="" srcset=""></button>
     </section>
 
