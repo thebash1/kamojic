ca3662fe4eac97bebb717f6046f2ab4dee4a84cf
Alex Daniel Mora Heredia <alex.daniel50904@gmail.com>
2024-09-24 18:49:05 -0500

    a modification more

--- php/view/addView.php
+++ php/view/addView.php
@@ -4,35 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Añadir</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <link rel="shortcut icon" href="../../icons/user-plus-regular-24.png" type="image/x-icon">
-    
+    <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
+
 </head>
 <body>
     <div class="container">
-        <div class="container-center">
-            <h1>Agendar citas</h1>
+        <div class="row">
+            <h1 class="text-center my-3">Agendar citas</h1>
             <nav class="navbar">
-                <ul class="items">
-                    <li><a href="./deleteView.php">Borrar cita</a></li>
-                    <li><a href="./updateView.php">Modificar cita</a></li>
-                    <li><a href="./searchView.php">Buscar cita</a></li>
-                    <li class="logout"><a href="../../index.html">Cerrar sesión</a></li>
+                <ul class="nav-item">
+                    <li><a class="nav-link" href="./deleteView.php">Borrar cita</a></li>
+                    <li><a class="nav-link" href="./updateView.php">Modificar cita</a></li>
+                    <li><a class="nav-link" href="./searchView.php">Buscar cita</a></li>
+                    <li><a class="nav-link" id="logout" name="logout" href="../../index.html">Cerrar sesión</a></li>
                 </ul>
             </nav>
-            <form action="" class="form">
-                <label for="fname">Nombre:</label>
-                <input type="text" name="fname" id="fname"><br>
-                <label for="lname">Apellido:</label>
-                <input type="text" name="lname" id="lname"><br>
-                <label for="phone">Teléfono:</label>
-                <input type="text" name="phone" id="phone"><br>
-            </form>
-
-            <div class="btn-group">
-                <button class="btn-cancel" type="button">Cancelar</button>
-                <button class="btn-save" type="button">Guardar</button>
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Nombres" aria-label="Nombres">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Apellidos" aria-label="Apellidos">
+                </div>
             </div>
+            <div class="row my-2">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Teléfono" aria-label="Teléfono" name="" id="">
+                </div>
+                <div class="col">
+                    <label class="visually-hidden" for="autoSizingSelect">Preference</label>
+                    <select class="form-select" id="autoSizingSelect">
+                    <option selected>Sexo</option>
+                    <option value="1">Masculino</option>
+                    <option value="2">Femenino</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Servicio elegido" aria-label="Servicio" name="" id="">
+                </div>
+                <div class="col">
+                    <input class="form-control" type="text" placeholder="Agendamiento presencial" aria-label="Cita" disabled>
+                </div>
+            </div>
+            
 
             <!-- <div class="mode">
                 <button class="btn-mode" type="button" id="toggleButton"><img src="../../icons/moon-regular-24.png" alt=""></button>
--- php/view/script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById('toggleButton');
-
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    toggleButton.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode'));
-});
