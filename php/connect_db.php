c4df0bc9da3f45fd5c3d5d80b7f32cdbe5439b17
Alex Daniel Mora Heredia <alex.daniel50904@gmail.com>
2024-09-24 12:20:30 -0500

    connection from database enabled

--- php/connect_db.php
+++ php/connect_db.php
@@ -1,20 +1,22 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $db_name = "db_kamojic";
 $port = 3306;
 $path = "extension=/path/to/extension/mysqli.so";
 
+$connect_db = new mysqli($servername, $username, $password, $db_name);
+date_default_timezone_set('America/Bogota');
+$date = new DateTime();
 
-$conn = new mysqli($servername, $username, $password, $db_name);
 
-if(!$conn){
-    echo "Conexión fallida" . mysqli_connect_error() . "<br>";
+if(!$connect_db){
+    echo "Conexión fallida" . $connect_db->connect_error . "<br>";
 }
 
-echo "date " . date("d/m/y") . "<br>";
-echo "time" . date("h:i:sa") . "<br>";
-$date = date("d/m/y") ." ". date("i:h:sa") .",";
+echo "Conexión establecida" . "<br>";
 
-$connect_db->close();
+echo "date " . date("d/m/y") . "<br>";
+echo "time " . $date->format("H:i:s") . "<br>";
+$date = date("d/m/y") ." ". date("i:h:sa") .",";
