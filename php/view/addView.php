53639037bfb85b9b8783d76f626784016ce0b42b
Alex Daniel Mora Heredia <alex.daniel50904@gmail.com>
2024-09-27 16:43:03 -0500

    front add reservation completed

--- php/view/addView.php
+++ php/view/addView.php
@@ -13,46 +13,60 @@
     <div class="container">
         <div class="row">
             <h1 class="text-center my-3">Agendar citas</h1>
-            <nav class="navbar">
-                <ul class="nav-item">
-                    <li><a class="nav-link" href="./deleteView.php">Borrar cita</a></li>
-                    <li><a class="nav-link" href="./updateView.php">Modificar cita</a></li>
-                    <li><a class="nav-link" href="./searchView.php">Buscar cita</a></li>
-                    <li><a class="nav-link" id="logout" name="logout" href="../../index.html">Cerrar sesión</a></li>
+            <nav class="navbar mb-3">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item"><a class="nav-link text-body" href="./deleteView.php">Borrar cita</a></li>
+                    <li class="nav-item"><a class="nav-link text-body" href="./updateView.php">Modificar cita</a></li>
+                    <li class="nav-item"><a class="nav-link text-body" href="./searchView.php">Buscar cita</a></li>
+                    <li class="nav-item"><a class="nav-link text-body" id="logout" name="logout" href="../../index.html">Cerrar sesión</a></li>
                 </ul>
             </nav>
-            <div class="row">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Nombres" aria-label="Nombres">
+            <div class="row justify-content-center">
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Nombres" aria-label="Nombres" required>
                 </div>
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Apellidos" aria-label="Apellidos">
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Apellidos" aria-label="Apellidos" required>
                 </div>
             </div>
-            <div class="row my-2">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Teléfono" aria-label="Teléfono" name="" id="">
+            <div class="row my-2 justify-content-center">
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Teléfono" aria-label="Teléfono" name="" id="" required>
                 </div>
-                <div class="col">
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
                     <label class="visually-hidden" for="autoSizingSelect">Preference</label>
-                    <select class="form-select" id="autoSizingSelect">
-                    <option selected>Sexo</option>
-                    <option value="1">Masculino</option>
-                    <option value="2">Femenino</option>
+                    <select class="form-select" id="autoSizingSelect" required>
+                        <option selected>Sexo</option>
+                        <option value="1">Masculino</option>
+                        <option value="2">Femenino</option>
                     </select>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col">
-                    <input type="text" class="form-control" placeholder="Servicio elegido" aria-label="Servicio" name="" id="">
+            <div class="row justify-content-center">
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Servicio elegido" aria-label="Servicio" name="" id="" required>
                 </div>
-                <div class="col">
-                    <input class="form-control" type="text" placeholder="Agendamiento presencial" aria-label="Cita" disabled>
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
+                    <input class="form-control" type="text" placeholder="Agendamiento presencial" aria-label="Agendamiento" disabled>
+                </div>
+            </div>
+
+            <div class="row my-2 justify-content-center">
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Fecha registrada" aria-label="Fecha" disabled>
+                </div>
+                <div class="col d-xs-block col-md-6 col-lg-4 col-xl-4">
+                    <input type="text" class="form-control" placeholder="Cita pendiente" aria-label="Estado" disabled>
                 </div>
             </div>
             
-
+            <div class="row my-2 justify-content-center ">
+                <div class="d-grid gap-2 col-7 col-md-6 col-lg-4">
+                    <button type="button" class="btn btn-primary">Guardar</button>
+                    <button type="button" class="btn btn-danger">Cancelar</button>
+                </div>
+            </div>
             <!-- <div class="mode">
                 <button class="btn-mode" type="button" id="toggleButton"><img src="../../icons/moon-regular-24.png" alt=""></button>
             </div> -->
