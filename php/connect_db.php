6cc90f6522fb8aec545982c0a9b79ea485a813d6
thebash1 <alex.daniel50904@gmail.com>
2024-11-22 15:04:44 -0500

    file db connect change

--- php/connect_db.php
+++ php/connect_db.php
@@ -6,17 +6,17 @@ $db_name = "db_kamojic";
 $port = 3306;
 $path = "extension=/path/to/extension/mysqli.so";
 
-$connect_db = new mysqli($servername, $username, $password, $db_name);
-date_default_timezone_set('America/Bogota');
-$date = new DateTime();
-
-
-if(!$connect_db){
-    echo "Conexión fallida" . $connect_db->connect_error . "<br>";
+try{
+    $connect_db = new mysqli($servername, $username, $password, $db_name);
+    echo "conexión establecida" . "<br>";
+}
+catch(mysqli_sql_exception){
+    echo "conexión no establecida" . "<br>";
 }
 
-echo "Conexión establecida" . "<br>";
+// date_default_timezone_set('America/Bogota');
+// $date = new DateTime();
 
-echo "date " . date("d/m/y") . "<br>";
-echo "time " . $date->format("H:i:s") . "<br>";
-$date = date("d/m/y") ." ". date("i:h:sa") .",";
+// echo "date " . date("d/m/y") . "<br>";
+// echo "time " . $date->format("H:i:s") . "<br>";
+// $date = date("d/m/y") ." ". date("i:h:sa") .",";
