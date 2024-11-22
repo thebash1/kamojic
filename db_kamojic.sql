d1f43a25722b83f3bf98d23626ba23aa3ea404f3
Alex Daniel Mora <125604546+thebash1@users.noreply.github.com>
2024-11-22 15:09:30 -0500

    sql code database local

---
+++ db_kamojic.sql
@@ -0,0 +1,341 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-11-2024 a las 21:08:27
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_kamojic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `register_user` varchar(20) NOT NULL,
+  `password_user` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `rol_name` varchar(20) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `customers`
+--
+
+CREATE TABLE `customers` (
+  `id_customer` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `register_user` varchar(20) NOT NULL,
+  `password_user` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `rol_name` varchar(20) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `employees`
+--
+
+CREATE TABLE `employees` (
+  `id_employee` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `register_user` varchar(20) NOT NULL,
+  `password_user` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `rol_name` varchar(20) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `payments`
+--
+
+CREATE TABLE `payments` (
+  `id_payment` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_reservation` int(11) DEFAULT NULL,
+  `id_service` int(11) DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `requested_form` varchar(20) NOT NULL,
+  `service_name` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` decimal(4,2) NOT NULL,
+  `time_scale` time NOT NULL DEFAULT curtime(),
+  `payment_method` varchar(20) NOT NULL,
+  `payment_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id_reservation` int(11) NOT NULL,
+  `id_employee` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_service` int(11) DEFAULT NULL,
+  `id_payment` int(11) DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `payment_method` varchar(20) NOT NULL,
+  `requested_form` varchar(20) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id_rol` int(11) NOT NULL,
+  `rol_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id_rol`, `rol_name`) VALUES
+(1, 'administrador'),
+(2, 'empleado'),
+(3, 'cliente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `services`
+--
+
+CREATE TABLE `services` (
+  `id_service` int(11) NOT NULL,
+  `service_name` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `time_scale` time NOT NULL DEFAULT curtime(),
+  `price` decimal(4,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `id_rol` int(11) DEFAULT NULL,
+  `register_user` varchar(20) NOT NULL,
+  `password_user` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `rol_name` varchar(20) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD KEY `fk_admin_rol` (`id_rol`),
+  ADD KEY `fk_admin_user` (`id_user`);
+
+--
+-- Indices de la tabla `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id_customer`),
+  ADD KEY `fk_customers_rol` (`id_rol`),
+  ADD KEY `fk_customers_user` (`id_user`);
+
+--
+-- Indices de la tabla `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id_employee`),
+  ADD KEY `fk_employees_rol` (`id_rol`),
+  ADD KEY `fk_employees_user` (`id_user`);
+
+--
+-- Indices de la tabla `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id_payment`),
+  ADD KEY `fk_payments_customer` (`id_customer`),
+  ADD KEY `fk_payments_reservation` (`id_reservation`),
+  ADD KEY `fk_payments_service` (`id_service`);
+
+--
+-- Indices de la tabla `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id_reservation`),
+  ADD KEY `fk_reservations_service` (`id_service`),
+  ADD KEY `fk_reservations_employee` (`id_employee`),
+  ADD KEY `fk_reservations_payment` (`id_payment`),
+  ADD KEY `fk_reservations_customer` (`id_customer`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id_service`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `fk_users_rol` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id_customer` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id_employee` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id_payment` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `services`
+--
+ALTER TABLE `services`
+  MODIFY `id_service` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `fk_admin_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`),
+  ADD CONSTRAINT `fk_admin_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Filtros para la tabla `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `fk_customers_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`),
+  ADD CONSTRAINT `fk_customers_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Filtros para la tabla `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `fk_employees_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`),
+  ADD CONSTRAINT `fk_employees_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Filtros para la tabla `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `fk_payments_customer` FOREIGN KEY (`id_customer`) REFERENCES `reservations` (`id_customer`),
+  ADD CONSTRAINT `fk_payments_reservation` FOREIGN KEY (`id_reservation`) REFERENCES `reservations` (`id_reservation`),
+  ADD CONSTRAINT `fk_payments_service` FOREIGN KEY (`id_service`) REFERENCES `reservations` (`id_service`);
+
+--
+-- Filtros para la tabla `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `fk_reservations_customer` FOREIGN KEY (`id_customer`) REFERENCES `customers` (`id_customer`),
+  ADD CONSTRAINT `fk_reservations_employee` FOREIGN KEY (`id_employee`) REFERENCES `employees` (`id_employee`),
+  ADD CONSTRAINT `fk_reservations_payment` FOREIGN KEY (`id_payment`) REFERENCES `payments` (`id_payment`),
+  ADD CONSTRAINT `fk_reservations_service` FOREIGN KEY (`id_service`) REFERENCES `services` (`id_service`);
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_users_rol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
